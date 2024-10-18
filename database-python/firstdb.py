2801c840aeb5e018b338724814b2c20b986b301d
Aditya-Navale-03 <aditya.navale.399@gmail.com>
2024-10-18 18:57:03 +0530

    programs are updated

--- database-python/firstdb.py
+++ database-python/firstdb.py
@@ -1,2 +1,33 @@
-import psycopg as pg
-pg.connect 
+import psycopg2
+
+try:
+    # Establish the connection
+    con = psycopg2.connect(
+        dbname="mysql",
+        user="postgres",
+        password="Ams@adi2",
+        host="localhost",
+        port="5432"  # Default is 5432
+    )
+
+    # Create a cursor object
+    cur = con.cursor()
+
+    #write query of create table students
+    cur.query= (
+        """create table students(name varhar, rollno int primary )"""
+    )
+
+    # Execute a simple query
+    cur.execute()
+
+except (Exception, psycopg2.Error) as error:
+    print("Error connecting to database:", error)
+
+
+    # Close the connection
+finally:
+    if con:
+     cur.close()
+     con.close()
+     print("PostgreSQL connection is closed")
